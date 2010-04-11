17d21ef92e2d810b569e7c967a9a4e343997ff9e
pdc <pdc@alleged.org.uk>
2010-04-11 10:24:12 +0100

    Removed a commented out fossil.

--- snaptioner/urls.py
+++ snaptioner/urls.py
@@ -10,6 +10,4 @@ urlpatterns = patterns('alleged.snaptioner.views',
     (r'^$', 'album_list', essential_args, 'album_list'),
     (r'^(?P<album_name>[a-z0-9-]+)/$', 'album_detail', essential_args, 'album_detail'),
     (r'^(?P<album_name>[a-z0-9-]+)/(?P<image_name>[a-z0-9.-]+)$', 'image_detail', essential_args, 'image_detail')
-    ##,
-##    (r'^albums/(?P<album_name>[a-z0-9-]+)/(?P<image_name>[a-z0-9]+).(?P<max_width>[0-9]+)x(?P<max_height>[0-9]+)$', 'image_thumbnail', essential_args, 'image_thumbnail')
 )
