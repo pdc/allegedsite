9b8dc60f6f7b46d174fb1aca5a26a6cc6aa3151f
pdc <pdc@alleged.org.uk>
2011-06-18 19:36:38 +0100

    Minecraft overview update

--- static/minecraft-overviews/1/markers.js
+++ static/minecraft-overviews/1/markers.js
@@ -1,1 +1,3 @@
-var markerData=[{"msg": "Spawn", "y": 67, "x": 45, "chunk": [13, 8], "z": -136, "type": "spawn"}]
+overviewer.collections.markerDatas.push([
+{"msg": "Spawn", "y": 64, "x": 303, "chunk": [15, 13], "z": -403, "type": "spawn"}
+]);
---
+++ static/minecraft-overviews/spufco/markers.js
@@ -0,0 +1,3 @@
+overviewer.collections.markerDatas.push([
+{"msg": "Spawn", "y": 66, "x": 38, "chunk": [6, 10], "z": -102, "type": "spawn"}
+]);
