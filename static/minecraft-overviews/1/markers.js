55da3ca74e16d00d086aaf5b4441fd1e06bd32ff
pdc <pdc@alleged.org.uk>
2011-09-04 11:00:04 +0100

    Remove Minecraft Overviews from git repo

--- static/minecraft-overviews/1/markers.js
+++
@@ -1,3 +0,0 @@
-overviewer.collections.markerDatas.push([
-{"msg": "Spawn", "y": 64, "x": 303, "chunk": [15, 13], "z": -403, "type": "spawn"}
-]);
--- static/minecraft-overviews/july/markers.js
+++
@@ -1,3 +0,0 @@
-overviewer.collections.markerDatas.push([
-{"msg": "Spawn", "y": 64, "x": -150, "chunk": [10, 2], "z": 2, "type": "spawn"}
-]);
