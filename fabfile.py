d4203e217145cc5ad7a70448a0eeaced80b5cd62
pdc <pdc@alleged.org.uk>
2012-11-07 17:04:25 +0000

    Deploy with Fabric

---
+++ fabfile.py
@@ -0,0 +1,41 @@
+# -*-coding: UTF-8 -*-
+# Run these commands with fab
+
+from fabric.api import local, settings, abort, run, cd, env, sudo, prefix
+from fabric.contrib.console import confirm
+
+env.hosts = ['alleged@spreadsite.org']
+env.site_name = 'alleged'
+env.virtualenv = env.site_name
+env.settings_subdir = env.site_name
+env.django_apps = ['blog', 'snaptioner', 'frontpage']
+
+def update_requirements():
+    local("pip freeze | egrep -v 'Fabric|pycrypto|ssh' > REQUIREMENTS")
+
+def test():
+    with settings(warn_only=True):
+        result = local('./manage.py test {0}'.format(' '.join(env.django_apps)), capture=True)
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
+
+def push():
+    local('git push')
+
+def deploy():
+    test()
+    push()
+
+    run('if [ ! -d static ]; then mkdir static; fi')
+    run('if [ ! -d cache ]; then mkdir cache; fi')
+
+    code_dir = '/home/{0}/Sites/{0}'.format(env.site_name)
+    with cd(code_dir):
+        run('git pull')
+        run('cp {0}/settings_production.py {0}/settings.py'.format(env.settings_subdir))
+
+        with prefix('. /home/{0}/virtualenvs/{1}/bin/activate'.format(env.site_name, env.virtualenv)):
+            run('pip install -r REQUIREMENTS')
+            run('./manage.py collectstatic --noinput')
+
+    run('touch /etc/uwsgi/emperor.d/{0}.ini'.format(env.site_name))
