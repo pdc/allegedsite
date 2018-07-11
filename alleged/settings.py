bd3130b57975f7ddbb2d491204b32d5fa30bf0de
Damian Cugley <pdc@alleged.org.uk>
2018-07-11 10:25:32 +0100

    Use django-environ to get settings from environment
    Avoid using a separate production settings file checked in to Git.

--- alleged/settings.py
+++ alleged/settings.py
@@ -2,19 +2,24 @@
 
 import os
 
-project_root = os.path.dirname(os.path.dirname(__file__))
+import environ
+env = environ.Env(
+    DEBUG=(bool, False),
+    STATIC_ROOT=(str, None),
+    STATIC_URL=(str, None),
+)
+expand_path = environ.Path(__file__) - 2
+environ.Env.read_env(expand_path('.env'))
 
-
-def expand_path(partial_path):
-    return os.path.join(project_root, partial_path)
+DEBUG = env('DEBUG')
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = [
     'localhost:8000',
     'alleged.org.uk',
 ]
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
@@ -23,10 +28,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'NAME': 'a.db',
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
+    'default': env.db(default='sqlite:///%s' % expand_path('a.db')),
 }
 
 # Local time zone for this installation. Choices can be found here:
@@ -55,13 +57,25 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            # ... some options here ...
+        },
+    },
+]
+
+
 SNAPTIONER_LIBRARY_DIR = expand_path('albums')
 SNAPTIONER_LIBRARY_URL = 'http://static.alleged.org.uk/albums/'
 
 BLOG_DIR = expand_path('content/pdc')
 BLOG_CACHE_ENTRIES = False
 
-HTTPLIB2_CACHE_DIR = '/home/alleged/cache'
+HTTPLIB2_CACHE_DIR = '/Users/pdc/Library/Caches/allegedsite-http'
 FLICKR_ATOM_URL = 'http://api.flickr.com/services/feeds/photos_public.gne?id=14145351@N00&lang=en-us&format=atom'
 LIVEJOURNAL_ATOM_URL = 'http://damiancugley.livejournal.com/data/atom'
 YOUTUBE_ATOM_URL = 'http://gdata.youtube.com/feeds/base/users/damiancugley/uploads?alt=atom&v=2&orderby=published'
@@ -70,14 +84,18 @@ GITHUB_ATOM_URL = 'https://github.com/pdc.atom'
 STATICFILES_DIRS = (
     expand_path('static'),
 )
-STATIC_URL = 'http://static.alleged.org.uk/'
-STATIC_ROOT = '/home/alleged/static'
+
+if env('STATIC_ROOT'):
+    STATIC_URL = env('STATIC_URL', default='http://static.alleged.org.uk/')
+    STATIC_ROOT = env('STATIC_ROOT')  # e.g., '/home/alleged/static')
+else:
+    STATIC_URL = '/s/'
 
 CACHE_MIDDLEWARE_SECONDS = 30
 CACHE_MIDDLEWARE_KEY_PREFIX = 'alleged'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '0&^f^h^uwgxa&yj=6=^8+-_aa7+-co)8y9h1h6#tnz)4btgotf'
+SECRET_KEY = env('SECRET_KEY')
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.cache.UpdateCacheMiddleware',
